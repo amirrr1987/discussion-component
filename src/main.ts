a52300550c073e78ff4b0272730c3b0182287bfe
Amir Maghami <Maghami.A1987@gmail.com>
2023-01-26 18:02:31 +0330

    feat: install UnoCSS

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
-import './assets/main.css'
+// main.ts
+import 'uno.css'
+import '@unocss/reset/tailwind.css'
+import 'virtual:unocss-devtools'
 
 const app = createApp(App)
 
