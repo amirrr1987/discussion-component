957f632dd949872eb5b5101a2680f6addc19e702
Amir Maghami <Maghami.A1987@gmail.com>
2023-01-26 20:03:09 +0330

    bk: backup

---
+++ src/components/TheDiscussion.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        {{ props }}
+    </div>
+</template>
+
+<script setup lang="ts">
+// import type { IDiscussion }   from '@/models'
+const props = defineProps([])
+</script>
+
+<style scoped>
+
+</style>
